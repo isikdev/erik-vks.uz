157f2f8a8af51ad96d5f49831208e54cf35fe224
Искандар <105116560+isikdev@users.noreply.github.com>
2022-08-23 13:36:37 +0500

    100%

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$('.xits__cards').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    useTransform: true,
+});
+$('.banner__slider').slick({
+    infinite: true,
+    arrows: true,
+});
+$('#tel').inputmask("(99) 999-9999");
